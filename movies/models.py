19e5c18d181d52f1bd30714ad37bb15e8f9bc9f3
code-sum <yisugyeong@gmail.com>
2022-10-06 14:53:46 +0900

    05 - Movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+영화 목록 리스트
+- 영화 제목 (title)
+- 영화 줄거리 (summary)
+- 영화 상영 시간 (running_time)
+'''
+
+class Movie(models.Model):
+    title = models.CharField(max_length=20)
+    summary = models.TextField()
+    running_time = models.IntegerField()
