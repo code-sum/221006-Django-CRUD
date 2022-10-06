1d0a5ed62074682d22918d2fc52aa8a690c46bbe
code-sum <yisugyeong@gmail.com>
2022-10-06 17:23:11 +0900

    07 - CREATE

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MovieForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'movies/index.html')
+
+def create(request):
+    if request.method == 'POST':
+        movie_form = MovieForm(request.POST)
+        if movie_form.is_valid():
+            movie_form.save()
+            return redirect('movies:index')
+    else:
+        movie_form = MovieForm()
+    context = {
+        'movie_form': movie_form
+    }
+    return render(request, 'movies/new.html', context=context)
